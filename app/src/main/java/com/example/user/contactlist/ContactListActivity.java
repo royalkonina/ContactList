a8e59dd58b9ef6358ace1aaf14ef230a80caca35
royalkonina <royalkonina@gmail.com>
2016-11-22 17:50:19 +0400

    Added loading  contacts to the list. Needed to add loading ans showing full contact information

--- app/src/main/java/com/example/user/contactlist/ContactListActivity.java
+++ app/src/main/java/com/example/user/contactlist/ContactListActivity.java
@@ -1,13 +1,86 @@
 package com.example.user.contactlist;
 
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 
-public class ContactListActivity extends AppCompatActivity {
+public class ContactListActivity extends AppCompatActivity implements OnContactClickListener {
+  private static final int CONTACT_LOADER_ID = 1337;
+  private ContactsListAdapter adapter;
+  private ListView lvContacts;
+  private ProgressBar progressBar;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_contact_list);
+    setContentView(R.layout.a_contact_list);
+    progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+    lvContacts = (ListView) findViewById(R.id.contact_list_view);
+
+    setupCursorAdapter();
+    lvContacts.setAdapter(adapter);
+    getSupportLoaderManager().initLoader(CONTACT_LOADER_ID,
+            new Bundle(), contactsLoader);
+  }
+
+  private void setupCursorAdapter() {
+    String[] uiBindFrom = {ContactsContract.Data.DISPLAY_NAME};
+    int[] uiBindTo = {R.id.name_view};
+    adapter = new ContactsListAdapter(
+            this, R.layout.i_listitem,
+            null, uiBindFrom, uiBindTo,
+            0);
+  }
+
+  private LoaderManager.LoaderCallbacks<Cursor> contactsLoader =
+          new LoaderManager.LoaderCallbacks<Cursor>() {
+            @Override
+            public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+               String[] projectionFields = new String[]{ContactsContract.Contacts._ID, ContactsContract.Contacts.DISPLAY_NAME};
+              return new CursorLoader(ContactListActivity.this,
+                      ContactsContract.Contacts.CONTENT_URI,
+                      projectionFields,
+                      //null,
+                     // SELECTION,
+                      null,
+                      null,
+                      null
+              );
+            }
+
+            @Override
+            public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+              adapter.swapCursor(cursor);
+              if (cursor == null) {
+                //Some error occurred
+              } else if (cursor.getCount() < 1) {
+                ImageView noContactsImage = (ImageView) findViewById(R.id.stub_no_contacts);
+                progressBar.setVisibility(View.GONE);
+                noContactsImage.setVisibility(View.VISIBLE);
+              } else {
+                lvContacts.setVisibility(View.VISIBLE);
+                progressBar.setVisibility(View.GONE);
+              }
+            }
+
+            @Override
+            public void onLoaderReset(Loader<Cursor> loader) {
+              adapter.swapCursor(null);
+            }
+          };
+
+  @Override
+  public void onContactClick(int contactId) {
+    lvContacts.setVisibility(View.GONE);
+    progressBar.setVisibility(View.VISIBLE);
+
   }
 }
---
+++ app/src/main/java/com/example/user/contactlist/OnContactClickListener.java
@@ -0,0 +1,9 @@
+package com.example.user.contactlist;
+
+/**
+ * Created by User on 11/22/2016.
+ */
+
+public interface OnContactClickListener {
+  void onContactClick(int contactId);
+}
